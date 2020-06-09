10388166b405b034194b631f89e193dac47c76d4
mook <mookdang24@gmail.com>
2020-06-09 10:48:32 -0700

    created js scripts file
    Co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>
    Co-authored by: Janet Karpenske <janetkarpenske@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,21 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-  
+    let sentence = $("input#sentence").val();
+    const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+   
+    let sentenceArray = sentence.split('');
+    for(let index = 0; index < sentenceArray.length; index++) {
+      if (vowels.includes(sentenceArray[index])){
+        sentenceArray[index] = "-";
+      }      
+    }
+    
+    let result = sentenceArray.join("");
+
+    $(".output").text(result);
+    $(".output").show();
+
 
   });
 });
