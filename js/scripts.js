04688773caf4a92cb8be12ac3ae19741e9b97466
mook <mookdang24@gmail.com>
2020-06-09 11:12:08 -0700

    finall check
    Co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>
    Co-authored by: Janet Karpenske <janetkarpenske@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,18 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    let sentence = $("input#sentence").val();
+    let sentence = $("input#sentence").val(); 
     const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
    
-    let sentenceArray = sentence.split('');
+    let sentenceArray = sentence.split(''); 
     for(let index = 0; index < sentenceArray.length; index++) {
-      if (vowels.includes(sentenceArray[index])){
-        sentenceArray[index] = "-";
+      if (vowels.includes(sentenceArray[index])){ 
+        sentenceArray[index] = "-"; 
       }      
     }
     
-    let result = sentenceArray.join("");
+    let result = sentenceArray.join(""); 
 
     $(".output").text(result);
-    //$(".output").show();
   });
 });
